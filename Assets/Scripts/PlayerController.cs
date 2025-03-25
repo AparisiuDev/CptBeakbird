9ce09ff6939a2b3760bf9d8edbfb5fd972ce73ed
AparisiuDev <aparisiu.evolution@gmail.com>
2025-03-25 12:33:51 +0100

    Movement + Cam + Barco
    Scripts finalitzats de la CAM del MOVEMENT i implementat BETA del BARCO

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
 
     [Header("Movement Settings")]
     [SerializeField] private float walkSpeed = 5f;
+    [SerializeField] private float rotationSpeed;
 
     [Header("Input")]
     private float moveInput;
@@ -19,14 +20,10 @@ public class PlayerController : MonoBehaviour
         controller = GetComponent<CharacterController>();
     }
 
-    private void GroundMovement()
+    private void Update()
     {
-        Vector3 move = new Vector3(turnInput, 0, moveInput);
-
-        move.y = 0;
-        move *= walkSpeed;
-
-        controller.Move(move * Time.deltaTime);
+        InputManagement();
+        Movement();
     }
 
     private void Movement()
@@ -34,15 +31,26 @@ public class PlayerController : MonoBehaviour
         GroundMovement();
     }
 
-    private void Update()
-    {
-        InputManagement();
-        Movement();
-    }
-
     private void InputManagement()
     {
         moveInput = Input.GetAxis("Vertical");
         turnInput = Input.GetAxis("Horizontal");
+    }
+
+    private void GroundMovement()
+    {
+        Vector3 move = new Vector3(turnInput, 0, moveInput);
+
+        float magnitude = Mathf.Clamp01(move.magnitude) * walkSpeed;
+        move.Normalize();
+
+        controller.SimpleMove(move * magnitude);
+
+        //Rotation
+        if (move != Vector3.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
     }
 }
