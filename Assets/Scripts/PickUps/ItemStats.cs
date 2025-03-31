c73cfcf8f82ddfaf9a07a89a9cf699334735d4a2
AparisiuDev <aparisiu.evolution@gmail.com>
2025-03-31 14:14:48 +0200

    Detecta si es Item i agafa els Stats
    Afegit un script que detecta si té el tag "Items" i guarda en una variable el seu Nom, Descripció, Valor, i Satisfacció.

---
+++ Assets/Scripts/PickUps/ItemStats.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemStats : MonoBehaviour
+{
+    [Header("Names and Description")]
+    public string Name;
+    public string Description;
+    [Header("Price and Satisfaction")]
+    public float price;
+    public float satisfaction;
+}
