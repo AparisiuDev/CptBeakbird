1fdf14566a13bcbd932635526480905c252f5626
SunlightKoa <judithcodime@gmail.com>
2025-06-13 13:47:33 +0200

    menumenumenumenu
    menu

--- Assets/Scripts/UI/OptionsManager.cs
+++ Assets/Scripts/UI/OptionsManager.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class OptionsManager : MonoBehaviour
 {
     public GameObject menuPanel; // Assign your UI GameObject here in the Inspector
+    public GameObject optionsPanel; // Assign your UI GameObject here in the Inspector
 
     void Update()
     {
@@ -15,5 +17,15 @@ public class OptionsManager : MonoBehaviour
                 menuPanel.SetActive(!menuPanel.activeSelf);
             }
         }
+    }
+
+    public void OpenOptions()
+    {
+        optionsPanel.SetActive(true);
+    }
+
+    public void CloseOptions()
+    {
+        optionsPanel.SetActive(false);
     }
 }
